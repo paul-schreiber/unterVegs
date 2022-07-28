08d59807aaa650a5adffac65721fc959cec4e912
Paul Schreiber <schreiber.paul96@gmail.com>
2022-07-29 01:06:53 +0200

    Update about and donate page

--- pages/donate.vue
+++ pages/donate.vue
@@ -1,3 +1,23 @@
 <template>
-    <div>Unterstütze unsere Arbeit mit einer Spende! 💸</div>
+    <div class="donate-page">
+        <h1>Sag Danke! 😊</h1>
+        <div>Du kannst uns gern unterstützen...</div>
+        <h3>Test</h3>
+    </div>
 </template>
+
+<style lang="scss" scoped>
+
+h1 {
+    width: fit-content;
+    border-bottom: 6px solid $color-secondary;
+}
+h3 {
+    width: fit-content;
+    border-bottom: 3px solid $color-secondary;
+}
+
+.donate-page {
+    text-align: left;
+}
+</style>
