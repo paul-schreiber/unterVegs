1bbf58c7780d1be5d9460a4e3c8708aebab230c5
Paul Schreiber <schreiber.paul96@gmail.com>
2022-07-29 14:57:27 +0200

    Add static ids

--- services/DataService.ts
+++ services/DataService.ts
@@ -1,27 +1,39 @@
-import products from '../data/products'
-import shops from '../data/shops'
+import productsJSON from '../data/products'
+import shopsJSON from '../data/shops'
 import { CategoryIds, Product, Shop, ShopIds } from '../types'
 
 export class DataService {
 
+    private products: Product[];
+    private shops: Shop[];
+
+    constructor() {
+        this.products = productsJSON.sort((a,b) => a.name.localeCompare(b.name))
+        this.shops = shopsJSON.sort((a,b) => a.name.localeCompare(b.name))
+
+        this.products.forEach(product => {
+            product.id = `${product.shop.replace(/\s/g,'')}-${product.name.replace(/\s/g,'')}`
+        })
+    }
+
     public getAllProducts(): Product[] {
-        return products.sort((a,b) => a.name.localeCompare(b.name))
+        return this.products
     }
 
     public getAllShops(): Shop[] {
-        return shops.sort((a,b) => a.name.localeCompare(b.name))
+        return this.shops
     }
 
     public getProductById(productId: String): Product {
-        return this.getAllProducts().find(product => product.id === productId)
+        return this.products.find(product => product.id === productId)
     }
 
     public getShopById(shopId: ShopIds): Shop {
-        return this.getAllShops().find(shop => shop.id === shopId)
+        return this.shops.find(shop => shop.id === shopId)
     }
 
     public getProductsByShopId(shopId: ShopIds): Product[] {
-        return this.getAllProducts().filter(product => product.shop === shopId)
+        return this.products.filter(product => product.shop === shopId)
     }
 
     public getCategoriesByShopId(shopId: ShopIds): CategoryIds[] {
@@ -32,7 +44,7 @@ export class DataService {
     }
 
     public filterProducts(searchTerm: string, filters: CategoryIds[]) {
-        return this.getAllProducts().filter((product) => {
+        return this.products.filter((product) => {
             const productName = product.name.toLowerCase()
             const hasMatchedSearchTerm = productName.includes(searchTerm.toLowerCase())
             const hasCategory = filters.length ? filters.some(filterCategory => product.categories.includes(filterCategory)) : true
@@ -41,7 +53,7 @@ export class DataService {
     }
 
     public filterShops(searchTerm: string, filters: CategoryIds[]) {
-        return this.getAllShops().filter((shop) => {
+        return this.shops.filter((shop) => {
             const shopName = shop.name.toLowerCase()
             const hasMatchedSearchTerm = shopName.includes(searchTerm.toLowerCase())
             const hasCategory = filters.length ? filters.some(filterCategory => this.getCategoriesByShopId(shop.id).includes(filterCategory)) : true
