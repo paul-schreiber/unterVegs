6cb4b1326a1dfd5a764bc62ae82473a9913ef5c4
Paul Schreiber <schreiber.paul96@gmail.com>
2022-08-08 12:42:30 +0200

    Add random welcome message

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="welcome-container">
-        <h1>Ist das Eis vegan?</h1>
+        <h1>{{ randomMessage }}</h1>
         <section>
             <p>Diese und weitere Fragen halten dich Tag und Nacht wach?</p>
             <p> Dann haben wir was für dich:</p>
@@ -16,6 +16,24 @@
         <HelpButton />
     </div>
 </template>
+
+<script lang="ts">
+export default defineComponent({
+    data() {
+        return {
+            welcomeMessages: ['Ist das Eis vegan?', 'Wo gibts vegane Nuggets?', 'Hat BK nur Pommes?']
+        }
+    },
+    computed: {
+        randomMessage() {
+            const randomIndex = Math.floor(Math.random() * this.welcomeMessages.length)
+            const message = this.welcomeMessages[randomIndex]
+            return `${message}`
+
+        }
+    }
+})
+</script>
 
 <style lang="scss" scoped>
 .welcome-container {
