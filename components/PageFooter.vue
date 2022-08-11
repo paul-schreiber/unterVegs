c0c7651959709bc30a3ba6afc71c2beef2f60484
Paul Schreiber <schreiber.paul96@gmail.com>
2022-08-11 16:53:16 +0200

    Add logo to footer

--- components/PageFooter.vue
+++ components/PageFooter.vue
@@ -1,15 +1,20 @@
 <template>
   <div class="footer">
     <div class="inner-footer">
-      <div class="insta-link-container">
-        <a :href="socialInfo.instagram" aria-label="Link zu Instagram">
-          <font-awesome-icon :icon="['fab', 'instagram']" />
-        </a>
+      <div class="logo">
+        <img src="~/assets/img/logo-white-unterVegs.svg" alt="unterVegs Logo" />
       </div>
-      <NuxtLink to="/legal">Impressum </NuxtLink>
-      <NuxtLink to="/privacy">Datenschutz</NuxtLink>
-      <div>
-        Made with ❤️ by Miri & <a class="contact-link" href="https://github.com/paul-schreiber">Paul</a>
+      <div class="sub-footer">
+        <div class="insta-link-container">
+          <a :href="socialInfo.instagram" aria-label="Link zu Instagram">
+            <font-awesome-icon :icon="['fab', 'instagram']" />
+          </a>
+        </div>
+        <NuxtLink to="/legal">Impressum </NuxtLink>
+        <NuxtLink to="/privacy">Datenschutz</NuxtLink>
+        <div>
+          Made with ❤️ by Miri & <a class="contact-link" href="https://github.com/paul-schreiber">Paul</a>
+        </div>
       </div>
     </div>
   </div>
@@ -34,24 +39,47 @@ export default defineComponent({
   background-color: $color-background-dark;
 
   .inner-footer {
+
     max-width: $max-page-width;
     margin: auto;
-    min-height: $footer-height;
-    color: $color-font-light;
-    padding: $sp-small $sp-big;
-    display: flex;
-    gap: $sp-small;
-    justify-content: space-between;
-    align-items: center;
-    flex-wrap: wrap;
 
-    a {
+    .sub-footer {
+      min-height: $footer-height;
       color: $color-font-light;
+      padding: $sp-small $sp-big;
+      display: flex;
+      gap: $sp-small;
+      justify-content: space-between;
+      align-items: center;
+      flex-wrap: wrap;
+
+      a {
+        color: $color-font-light;
+      }
+
+      .insta-link-container {
+        font-size: $fs-large;
+        vertical-align: center;
+      }
     }
 
-    .insta-link-container {
-      font-size: $fs-large;
-      vertical-align: center;
+    .logo {
+      padding: $sp-big 0px;
+
+      img {
+        height: 100px;
+        cursor: pointer;
+        user-select: none;
+        /* supported by Chrome and Opera */
+        -webkit-user-select: none;
+        /* Safari */
+        -khtml-user-select: none;
+        /* Konqueror HTML */
+        -moz-user-select: none;
+        /* Firefox */
+        -ms-user-select: none;
+        /* Internet Explorer/Edge */
+      }
     }
   }
 }
