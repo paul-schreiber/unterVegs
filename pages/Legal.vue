a2dca0a6b7557021b117d0a056d4c850c8b96cbb
Paul Schreiber <schreiber.paul96@gmail.com>
2022-08-13 18:58:00 +0200

    Add email link

--- pages/Legal.vue
+++ pages/Legal.vue
@@ -12,23 +12,21 @@
                     <div>{{ address.zipCode }} {{ address.city }}</div>
 
                     <div class="mt-s">
-                        {{ mail }}
+                        <a :href="`mailto:${mail}`">{{ mail }}</a>
                     </div>
                 </address>
             </section>
             <h3>Haftung für Inhalte</h3>
 
             <section>
-                Als Diensteanbieter sind wir gemäß § 7 Abs.1 TMG für eigene Inhalte
-                auf diesen Seiten nach den allgemeinen Gesetzen verantwortlich. Nach
+                Als Diensteanbieter sind wir gemäß § 7 Abs.1 TMG für eigene Inhalte                auf diesen Seiten nach den allgemeinen Gesetzen verantwortlich. Nach
                 §§ 8 bis 10 TMG sind wir als Diensteanbieter jedoch nicht
                 verpflichtet, übermittelte oder gespeicherte fremde Informationen zu
                 überwachen oder nach Umständen zu forschen, die auf eine rechtswidrige
                 Tätigkeit hinweisen. Verpflichtungen zur Entfernung oder Sperrung der
                 Nutzung von Informationen nach den allgemeinen Gesetzen bleiben
                 hiervon unberührt. Eine diesbezügliche Haftung ist jedoch erst ab dem
-                Zeitpunkt der Kenntnis einer konkreten Rechtsverletzung möglich. Bei
-                Bekanntwerden von entsprechenden Rechtsverletzungen werden wir diese
+                Zeitpunkt der Kenntnis einer konkreten Rechtsverletzung möglich. Bei                Bekanntwerden von entsprechenden Rechtsverletzungen werden wir diese
                 Inhalte umgehend entfernen.
             </section>
 
@@ -100,6 +98,10 @@ export default defineComponent({
         font-weight: $fw-light;
         line-height: 1.6rem;
         margin-bottom: $sp-large;
+    }
+
+    a {
+        color: $color-font-dark;
     }
 }
 </style>
