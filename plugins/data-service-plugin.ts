87c682443a3b539150360b940ad738d12b5ba55b
Paul Schreiber <schreiber.paul96@gmail.com>
2022-10-13 13:00:55 +0200

    Polishing

--- plugins/data-service-plugin.ts
+++ plugins/data-service-plugin.ts
@@ -1,9 +1,8 @@
 import { DataService } from '../services/DataService'
 export default defineNuxtPlugin(() => {
-    return {
-      provide: {
-        DS: new DataService()
-      }
+  return {
+    provide: {
+      DS: new DataService()
     }
-  })
-  
+  }
+})
